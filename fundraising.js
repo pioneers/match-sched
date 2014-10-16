20a8e804584140bf1d0db829764050077b34c084
William Li <wli@williamhli.com>
2014-10-16 15:58:47 -0700

    Formatting.

--- fundraising.js
+++ fundraising.js
@@ -1,53 +1,53 @@
 function init() {
-    /* set up XMLHttpRequest */
-    var url = "fundraising.xlsx";
-    var oReq = new XMLHttpRequest();
-    oReq.open("GET", url, true);
-    oReq.responseType = "arraybuffer";
+  /* set up XMLHttpRequest */
+  var url = "fundraising.xlsx";
+  var oReq = new XMLHttpRequest();
+  oReq.open("GET", url, true);
+  oReq.responseType = "arraybuffer";
 
-    oReq.onload = function(e) {
-        var arraybuffer = oReq.response;
+  oReq.onload = function(e) {
+    var arraybuffer = oReq.response;
 
-        /* convert data to binary string */
-        var data = new Uint8Array(arraybuffer);
-        var arr = new Array();
-        for(var i = 0; i != data.length; ++i) arr[i] = String.fromCharCode(data[i]);
-        var bstr = arr.join("");
+    /* convert data to binary string */
+    var data = new Uint8Array(arraybuffer);
+    var arr = new Array();
+    for(var i = 0; i != data.length; ++i) arr[i] = String.fromCharCode(data[i]);
+    var bstr = arr.join("");
 
-        /* Call XLSX */
-        var workbook = XLSX.read(bstr, {type:"binary"});
-        parseWorkbook(workbook);
-    }
+    /* Call XLSX */
+    var workbook = XLSX.read(bstr, {type:"binary"});
+    parseWorkbook(workbook);
+  }
 
-    oReq.send();
+  oReq.send();
 }
 
 function parseWorkbook(workbook) {
-    var data = workbook['Sheets']['website'];
-    var goal = data['B1']['w'];
-    var raised = data['B2']['w'];
-    var togo = data['B3']['w'];
+  var data = workbook['Sheets']['website'];
+  var goal = data['B1']['w'];
+  var raised = data['B2']['w'];
+  var togo = data['B3']['w'];
 
-    $('#summary').text('$' + raised + ' out of $' + goal);
-    $('#goal').text('$' + goal);
+  $('#summary').text('$' + raised + ' out of $' + goal);
+  $('#goal').text('$' + goal);
 
-    var transitionDuration = Math.min(Math.max(1, (4 * raised / goal)), 4);
+  var transitionDuration = Math.min(Math.max(1, (4 * raised / goal)), 4);
 
-    $('#chart-label-25').text('$' + goal/4);
-    $('#chart-label-50').text('$' + goal/4 * 2);
-    $('#chart-label-75').text('$' + goal/4 * 3);
-    $('#chart-label-100').text('$' + goal);
-    $('#chart-labels div').addClass('visible');
+  $('#chart-label-25').text('$' + goal/4);
+  $('#chart-label-50').text('$' + goal/4 * 2);
+  $('#chart-label-75').text('$' + goal/4 * 3);
+  $('#chart-label-100').text('$' + goal);
+  $('#chart-labels div').addClass('visible');
 
-    var raisedHeight = Math.min(400, 400 * raised / goal);
-    $('#chart-raised').css('height', raisedHeight + 'px').css('transition-duration', transitionDuration + 's').addClass('visible');
-    $('#chart-raised-label').text('$' + raised).css('transition-delay', (transitionDuration + 1) + 's');
+  var raisedHeight = Math.min(400, 400 * raised / goal);
+  $('#chart-raised').css('height', raisedHeight + 'px').css('transition-duration', transitionDuration + 's').addClass('visible');
+  $('#chart-raised-label').text('$' + raised).css('transition-delay', (transitionDuration + 1) + 's');
 
-    var individualDonations = data['B9']['w'];
-    var averageDonation = data['B10']['w'];
-    $('#individual-donations').text(individualDonations);
-    $('#average-donation').text(averageDonation);
-    $('#donation-metadata').addClass('visible').css('transition-delay', (transitionDuration + 1.5) + 's');
+  var individualDonations = data['B9']['w'];
+  var averageDonation = data['B10']['w'];
+  $('#individual-donations').text(individualDonations);
+  $('#average-donation').text(averageDonation);
+  $('#donation-metadata').addClass('visible').css('transition-delay', (transitionDuration + 1.5) + 's');
 }
 
 $(function() {
